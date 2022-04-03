6b17cd63ee3e45a2357f1ac166efda85aa1460f4
Marodin Shahbaz Yourghanlo <shahbazm@oregonstate.edu>
2022-04-02 18:16:03 -0700

    generate password and configured cache

--- config/config.py
+++ config/config.py
@@ -8,9 +8,17 @@ load_dotenv()
 class Config(object):
     FLASK_APP = environ.get('FLASK_APP')
     SECRET_KEY = environ.get('SECRET_KEY')
+
+    # session
     SESSION_TYPE = 'redis'
     SESSION_PERMANENT = False
     SESSION_USE_SIGNER = True
+
+    # cache
+    CACHE_TYPE = 'RedisCache'
+    CACHE_REDIS_HOST = environ.get('CACHE_REDIS_HOST')
+    CACHE_REDIS_PORT = environ.get('CACHE_REDIS_PORT')
+    CACHE_REDIS_URL = environ.get('CACHE_REDIS_URL')
 
 
 class Production(Config):
--- main.py
+++ main.py
@@ -1,16 +1,28 @@
-from flask import Flask
+from flask import Flask, request
+from flask_session import Session
 from flask_socketio import SocketIO, join_room, leave_room, send
 from config.init_config import init_config
+from flask_caching import Cache
+from anonfiles.models.generate_pass import generate_pass
 
 app = Flask(__name__)
+
 get_config = init_config()
 app.config.from_object(get_config)
+
 io = SocketIO(app)
+
+sess = Session()
+cache = Cache(app)
+
+sess.init_app(app)
 
 
 @app.route("/")
-def root():
-    pass
+def create_room():
+    room_pass = generate_pass()
+    # cache room_name:password
+    return room_pass
 
 
 @io.on('join', namespace='/room')
